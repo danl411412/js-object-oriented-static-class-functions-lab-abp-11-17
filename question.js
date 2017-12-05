b1cfdfd94a9fdbf8018b325b837346610038bdb7
danl411412 <daan.b.lee@gmail.com>
2017-12-05 09:10:23 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,21 @@
 'use strict';
+class Question {
+  constructor(question){
+    this.content = question;
+    this.save();
+  }
+
+  static All() {
+    return this._All;
+  }
+
+  save() {
+    return this.constructor._All.push(this);
+  }
+
+  static Find(questionIndex) {
+    return this.All()[questionIndex - 1];
+  }
+}
+
+Question._All = [];
